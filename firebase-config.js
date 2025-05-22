827c992b5141b03efa0d027bb7a63a6b09b86e77
Valen <valentains.pomponio@gamil.com>
2025-05-22 21:30:46 +0200

    heloow

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "google_api_key",
+  apiKey: "AIzaSyAl3a_MpU3JviVdf4oEyaP3RWrYeSf6wDM",
   authDomain: "apuntaja.firebaseapp.com",
   projectId: "apuntaja",
   storageBucket: "apuntaja.firebasestorage.app",
   messagingSenderId: "956916894313",
-  appId: "1:956916894313:web:ecee75cde8feaee455f0f8"
+  appId: "1:956916894313:web:ecee75cde8feaee455f0f8",
+  measurementId: "G-NQ7L4E6P5X"
 };
-
-firebase.initializeApp(firebaseConfig);
